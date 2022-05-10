6bff49b2408edb97bc1931f7424296b7ed7e1560
Aman Sharma <shrma.aman7@gmail.com>
2022-05-10 15:35:31 +0530

    Initial Commit

---
+++ Notes WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Notes WatchKit Extension
+//
+//  Created by AMAN SHARMA on 10/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Notes WatchKit Extension/NotesApp.swift
@@ -0,0 +1,19 @@
+//
+//  NotesApp.swift
+//  Notes WatchKit Extension
+//
+//  Created by AMAN SHARMA on 10/05/22.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
